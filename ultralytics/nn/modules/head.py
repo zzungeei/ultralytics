e1177a574c6b7d378427a644449910b417d7ce3c
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-06-30 16:14:38 +0200

    merge main

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -70,7 +70,7 @@ class Detect(nn.Module):
             b[-1].bias.data[:m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)
 
 
-class Detect(nn.Module):
+class DetectNEW(nn.Module):
     """YOLOv8 Detect head for detection models."""
     dynamic = False  # force grid reconstruction
     export = False  # export mode
